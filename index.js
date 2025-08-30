b002bb9fe7b5a040dd3f5982cdbc678276cfa6db
Skipper <sharmanfaridz@gmail.com>
2025-08-30 12:55:21 +0800

    enable initialization of board

--- index.js
+++ index.js
@@ -8,7 +8,42 @@ function createPlayer(mark) {
   }
 }
 
-
 let player1 = createPlayer("X") 
 let player2 = createPlayer("O")
 console.log(player2.getMark())
+
+let board = (function() {
+  let noMarkValue = null
+  let height = 3
+  let width = 3
+  let boardArray = []
+
+  // boars shold be [
+  // [1,2,3]
+  // [4,5,6]
+  // [7,8,9]
+
+  initBoard = () => {
+    boardArray = []
+    for(let i = 0; i < height; i++) {
+      let row = []
+      for(let j = 0; j < width; j++) {
+        row.push(noMarkValue)
+      }
+      boardArray.push(row)
+    }
+  }
+
+  getBoard = () => {
+    return boardArray
+  }
+
+  return {
+    getBoard,
+    initBoard
+  }
+
+})()
+
+board.initBoard()
+console.log(board.getBoard());
