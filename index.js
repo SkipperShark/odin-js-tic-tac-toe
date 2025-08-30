845110d1450ea286bdd9ff2c96ed132012a5170d
Skipper <sharmanfaridz@gmail.com>
2025-08-30 12:48:10 +0800

    get private variables for createPlayer factory function working

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-console.log("test");
+function createPlayer(mark) {
+  getMark = function() {
+    return mark
+  }
+
+  return {
+    getMark
+  }
+}
+
+
+let player1 = createPlayer("X") 
+let player2 = createPlayer("O")
+console.log(player2.getMark())
