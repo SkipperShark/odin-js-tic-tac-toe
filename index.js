55f6eb97eb028240aa3c05a0bf28335fb51c7f12
skippershark <sharmanfaridz@gmail.com>
2025-09-09 18:35:17 +0800

    change to tab size of 4, 2 is kinda hard to read

--- index.js
+++ index.js
@@ -8,97 +8,97 @@ let rlInput = process.stdin
 let rlOutput = process.stdout
 
 let log = (function () {
-  let log = console.log
-
-  let info = function(message) {
-    log(chalk.blueBright(message))
-  }
-
-  let debug = function(message) {
-    log(chalk.magentaBright(message))
-  }
-
-  let error = function(message) {
-    log(chalk.redBright(message))
-  }
-
-  return {
-    info, debug, error
-  }
+    let log = console.log
+    
+    let info = function(message) {
+        log(chalk.blueBright(message))
+    }
+    
+    let debug = function(message) {
+        log(chalk.magentaBright(message))
+    }
+    
+    let error = function(message) {
+        log(chalk.redBright(message))
+    }
+    
+    return {
+        info, debug, error
+    }
 })()
 
 
 function createPlayer(name, mark) {
-  let getMark =  () => mark
-  let getName = () => name
-
-  return {
-    getMark,
-    getName
-  }
+    let getMark =  () => mark
+    let getName = () => name
+    
+    return {
+        getMark,
+        getName
+    }
 }
 
 let board = (function() {
-  const _noMarkValue = null
-  const _height = 3
-  const _width = 3
-
-  let _cells = []
-  for(let i = 0; i < _height; i++) {
-    let row = []
-    for(let j = 0; j < _width; j++) {
-      row.push(_noMarkValue)
+    const _noMarkValue = null
+    const _height = 3
+    const _width = 3
+    const feBoard = document.getElementById("board")
+    
+    let _cells = []
+    for(let i = 0; i < _height; i++) {
+        let row = []
+        for(let j = 0; j < _width; j++) {
+            row.push(_noMarkValue)
+        }
+        _cells.push(row)
     }
-    _cells.push(row)
-  }
-
-  let setCellByCellNum = function(cellNum, mark) {
-    _setCell({..._getXAndYFromCellNum(cellNum), mark})
-  }
-  
-  // let printBoard = function() {
+    
+    render();
+    
+    function setCellByCellNum(cellNum, mark) {
+        _setCell({..._getXAndYFromCellNum(cellNum), mark})
+    }
+    
+    // let printBoard = function() {
     //   log.debug("");
     //   _cells.forEach((row) => {
-  //     console.log(row);
-  //   })
-  //   log.debug("");
-  // }
-  
-  let getCells = () => _cells.map(arr => [...arr])
-  let getHeight = () => _height
-  let getWidth = () => _width
-  let full = () => _cells.flat().filter(cell => cell === _noMarkValue).length === 0
-
-  render();
-
-  function render() {
-
-  }
-  
-  let _getXAndYFromCellNum = function(cellNum) {
-    let conv1BasedTo0Based = 0
-    let x = Math.floor(cellNum / _width) - conv1BasedTo0Based
-    let y = cellNum % _height - conv1BasedTo0Based
-    return {x, y}
-  }
-  
-  let _setCell = function({x, y, mark}) {
-    if (_cells[x] === undefined || _cells[x][y] === undefined) {
-      throw Error("You have chosen an invalid cell!")
+        //     console.log(row);
+    //   })
+    //   log.debug("");
+    // }
+    
+    function getCells() { return _cells.map(arr => [...arr]) }
+    function getHeight() { return _height }
+    function getWidth() { return _width }
+    function full() { return _cells.flat().filter(cell => cell === _noMarkValue).length === 0 }
+    function render() {
+        
     }
-    if (_cells[x][y] !== null) {
-      throw Error("Cell already has a mark!")
+    
+    function _getXAndYFromCellNum(cellNum) {
+        let conv1BasedTo0Based = 0
+        let x = Math.floor(cellNum / _width) - conv1BasedTo0Based
+        let y = cellNum % _height - conv1BasedTo0Based
+        return {x, y}
     }
-    _cells[x][y] = mark
-  }
-
-  return {
-    full,
-    setCellByCellNum,
-    getCells,
-    getHeight,
-    getWidth
-  }
+    
+    function _setCell({x, y, mark}) {
+        if (_cells[x] === undefined || _cells[x][y] === undefined) {
+            throw Error("You have chosen an invalid cell!")
+        }
+        if (_cells[x][y] !== null) {
+            throw Error("Cell already has a mark!")
+        }
+        _cells[x][y] = mark
+    }
+    
+    return {
+        full,
+        setCellByCellNum,
+        getCells,
+        getHeight,
+        getWidth
+    }
 })()
 
 // let consoleIOController = (function(rlInput, rlOutput) {
@@ -106,13 +106,13 @@ let board = (function() {
 //     input: rlInput,
 //     output: rlOutput,
 //   });
-  
+
 //   let promptUser = (message, inputHandler) => {
-//     rl.question(chalk.blueBright(message), inputHandler)
+    //     rl.question(chalk.blueBright(message), inputHandler)
 //   }
 
 //   let terminate = () => {
-//     rl.close()
+    //     rl.close()
 //   }
 
 //   return {
@@ -123,127 +123,127 @@ let board = (function() {
 
 
 let game = (function(player1Mark, player2Mark) {
-  let player1 = createPlayer("Mark", player1Mark) 
-  let player2 = createPlayer("John", player2Mark)
-  let player1Turn = true
-  // let board = createBoard()
-  
-  function start() {
-    log.info("Welcome to Odin Tic-tac-toe!")
-    playRound()
-  }
-  
-  function playRound() {
-    if (board.full() === true) {
-      _endGame({isTie: true})
-      return
+    let player1 = createPlayer("Mark", player1Mark) 
+    let player2 = createPlayer("John", player2Mark)
+    let player1Turn = true
+    // let board = createBoard()
+    
+    function start() {
+        log.info("Welcome to Odin Tic-tac-toe!")
+        playRound()
     }
-    let message = `${_curPlayer().name}'s turn, where would you `
-      + `like to put your ${_curPlayer().mark} mark?\n`;
-    // consoleIOController.promptUser(message, _inputHandler)
-  }
-
-  function _inputHandler(input) {
-    try {
-      board.setCellByCellNum(parseInt(input), _curPlayer().mark)
-      if (_winnerFound() === true) {
-        _endGame()
-        return
-      }
-      player1Turn = !player1Turn
-      playRound()
-      return
-    }
-    catch (error) {
-      log.error(error.message)
-      playRound()
-      return
-    }
-  }
-
-  function _winnerFound() {
-    let cells = board.getCells()
-    let bWidth = board.getWidth()
-    let bHeight = board.getHeight()
-
-    let computerWinner = function(groups) {
-      for(const group of groups) { 
-        let markCounts = group.reduce(
-          (acc, ele) => {
-            if (ele === null) return acc
-            acc[ele] === undefined ? acc[ele] = 1 : acc[ele] += 1
-            return acc
-          },
-          {}
-        )
-        if (Math.max(...Object.values(markCounts)) >= group.length) {
-          return true
+    
+    function playRound() {
+        if (board.full() === true) {
+            _endGame({isTie: true})
+            return
         }
-      }
-      return false
-    }
-    // win conditions
-    // straight horizontal line
-    if (computerWinner(cells) == true) {
-      return true
-    }
-
-    // straight vertical lines
-    let groupsVertical = []
-    for(let col_i = 0; col_i < board.getWidth(); col_i++) {
-      let col = []
-      for (let row_i = 0; row_i < board.getHeight(); row_i++) {
-        col.push(cells[row_i][col_i])
-      }
-      groupsVertical.push(col) 
-    }
-    if (computerWinner(cells) === true) {
-      return true
+        let message = `${_curPlayer().name}'s turn, where would you `
+        + `like to put your ${_curPlayer().mark} mark?\n`;
+        // consoleIOController.promptUser(message, _inputHandler)
     }
     
-    // diagonal line (top left to bottom right)
-    let groupsDiag = []
-    let diag = []
-    for(let i = 0; i < bWidth; i++) {
-      diag.push(cells[i][i])
+    function _inputHandler(input) {
+        try {
+            board.setCellByCellNum(parseInt(input), _curPlayer().mark)
+            if (_winnerFound() === true) {
+                _endGame()
+                return
+            }
+            player1Turn = !player1Turn
+            playRound()
+            return
+        }
+        catch (error) {
+            log.error(error.message)
+            playRound()
+            return
+        }
     }
-    groupsDiag.push(diag)
     
-    // diagonal line (bottom left to top right)
-    diag = []
-    for(let i = 0, j = bHeight - 1; i < bWidth && j < bHeight; i++, j--) {
-      diag.push(cells[j][i])
+    function _winnerFound() {
+        let cells = board.getCells()
+        let bWidth = board.getWidth()
+        let bHeight = board.getHeight()
+        
+        let computerWinner = function(groups) {
+            for(const group of groups) { 
+                let markCounts = group.reduce(
+                    (acc, ele) => {
+                        if (ele === null) return acc
+                        acc[ele] === undefined ? acc[ele] = 1 : acc[ele] += 1
+                        return acc
+                    },
+                    {}
+                )
+                if (Math.max(...Object.values(markCounts)) >= group.length) {
+                    return true
+                }
+            }
+            return false
+        }
+        // win conditions
+        // straight horizontal line
+        if (computerWinner(cells) == true) {
+            return true
+        }
+        
+        // straight vertical lines
+        let groupsVertical = []
+        for(let col_i = 0; col_i < board.getWidth(); col_i++) {
+            let col = []
+            for (let row_i = 0; row_i < board.getHeight(); row_i++) {
+                col.push(cells[row_i][col_i])
+            }
+            groupsVertical.push(col) 
+        }
+        if (computerWinner(cells) === true) {
+            return true
+        }
+        
+        // diagonal line (top left to bottom right)
+        let groupsDiag = []
+        let diag = []
+        for(let i = 0; i < bWidth; i++) {
+            diag.push(cells[i][i])
+        }
+        groupsDiag.push(diag)
+        
+        // diagonal line (bottom left to top right)
+        diag = []
+        for(let i = 0, j = bHeight - 1; i < bWidth && j < bHeight; i++, j--) {
+            diag.push(cells[j][i])
+        }
+        groupsDiag.push(diag)
+        
+        if (computerWinner(cells) === true) {
+            return true
+        }
+        
+        return false
     }
-    groupsDiag.push(diag)
     
-    if (computerWinner(cells) === true) {
-      return true
+    function _curPlayer() {
+        let player = player1Turn? player1 : player2
+        return {
+            mark: player.getMark(),
+            name: player.getName()
+        }
     }
-
-    return false
-  }
-  
-  function _curPlayer() {
-    let player = player1Turn? player1 : player2
+    
+    function _endGame({isTie=false} = {}) {
+        if (isTie) {
+            log.info(`Tie!, thanks for playing!`)
+        } else {
+            log.info(`Winner found! Congrats ${_curPlayer().name}!`)
+        }
+        consoleIOController.terminate()
+    }
+    
     return {
-      mark: player.getMark(),
-      name: player.getName()
+        start
     }
-  }
-  
-  function _endGame({isTie=false} = {}) {
-    if (isTie) {
-      log.info(`Tie!, thanks for playing!`)
-    } else {
-      log.info(`Winner found! Congrats ${_curPlayer().name}!`)
-    }
-    consoleIOController.terminate()
-  }
-
-  return {
-    start
-  }
-  
+    
 })(player1Mark, player2Mark)
 
 game.start()
